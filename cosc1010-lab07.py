6811f0ef35a208264a2fa56dd4a47ea89ff1fba3
hannahspicher <hspicher@uwyo.edu>
2024-10-30 00:55:03 +0000

    finished

--- cosc1010-lab07.py
+++ cosc1010-lab07.py
@@ -1,12 +1,14 @@
-# Your Name Here
+# Hannah Spicher
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section: 
-# Sources, people worked with, help given to: 
-# your
-# comments
-# here
+# 10/29/24
+# Lab 07
+# Lab Section: 11
+# Sources, people worked with, help given to: Textbook, Online Stack flow textbook, W3 schools textbook examples
+# I used .lstrip to remove the negative sign in part one even though it wasn't mentioned until part 2 because the
+# negative sign was causing the loop to return "please enter a numerical value" instead of "Please enter a positive 
+# "number."
+# Also in pt 3 I left the operand and operators defined inside of the while loop because it is re valued each time, but I 
+# wasn't sure if it would work with it outside of the while loop and if that would have been cleaner. 
 
 
 # Prompt the user for an upper bound 
@@ -16,8 +18,20 @@
     # To do so you can use the methods `.isdigit()` or `.isnumeric()`
     # If a user did not enter a number output a statement saying so
 # You will continue to prompt the user until a proper integer value is entered
-
+while True:
+    upperbound = input("Enter upper bound here:")
+    if not upperbound.lstrip('-').isdigit():
+        print("Please enter a numerical value.")
+    else:
+        upperbound=int(upperbound)
+        if upperbound <=0:
+            print("Please enter a positive number.")
+        else:
+            break  
 factorial = 1
+while upperbound > 1:
+    factorial *= upperbound
+    upperbound -= 1
 
 print(f"The result of the factorial based on the given bound is {factorial}")
 
@@ -36,8 +50,16 @@ print("*"*75)
     # I recommend checking out: https://www.w3schools.com/python/ref_string_replace.asp to figure out how one may remove a character from a string
 # All this together means you will have an intensive while loop that includes multiple if statements, likely with some nesting 
 # The sum should start at 0 
-
 num_sum = 0 
+while True:
+   message= input("Enter numbers here, or 'exit' to quit:") 
+   if message.lower() == 'exit':
+       break
+   elif message.lstrip('-').isdigit():
+        message=int(message)
+        num_sum= num_sum+message
+
+
 
 print(f"Your final sum is {num_sum}")
 
@@ -58,5 +80,38 @@ print("*"*75)
     # So, it should function the same for `5 + 6` as `5+6`
 # Print the result of the equation
 # Again, loop through prompting the user for input until `exit` in any casing is input 
+left_term=0
+right_term=0 
 
+while True:
+    calculate= input("Enter operand operator operand here or 'exit' to quit:")
+    if calculate.lower() == 'exit':
+        break
+    calculate = calculate.replace(" ","")
+    operator= None
+    operand= []
+    for operand in "+-/*%":
+        if operand in calculate:
+            operands = calculate.split(operand)
+            operator = operand
+
+    if operands[0].isdigit() and operands[1].isdigit():
+        left_term = int(operands [0])
+        right_term = int(operands[1])
+    else:
+        print(f"Please only enter numerical values")
+    
+    if operator == '+':
+        answer = left_term + right_term
+    elif operator == '-':
+        answer = left_term - right_term
+    elif operator == '/':
+        answer = left_term / right_term
+    elif operator == '*':
+        answer = left_term * right_term
+    elif operator == '%':
+        answer = left_term % right_term
+   
+        
+    print("Result:", answer)   
         
